040fb75208f31e4ea5e0a878d747cbf9d708a416
nahian255 <nahimn253@gmail.com>
2023-05-19 01:03:00 +0600

    upadated succesfully

--- index.js
+++ index.js
@@ -1,10 +1,95 @@
 const express = require('express');
 const app = express();
 const cors = require('cors')
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+
 
 
 app.use(cors())
 app.use(express.json())
+
+
+const uri = "mongodb+srv://toydb:LH8ct1ZUDFjoCCMC@cluster0.lyu30gb.mongodb.net/?retryWrites=true&w=majority";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+        const db = client.db("toySection");
+        const toyCollection = db.collection("toys");
+
+        // post toy function....
+        app.post('/addtoy', async (req, res) => {
+            const body = req.body
+            const result = await toyCollection.insertOne(body)
+            console.log(result);
+            if (result?.insertedId) {
+                return res.send(result);
+            } else {
+                return res.status(404).send({
+                    message: "can not insert try again leter",
+                    status: false,
+                });
+            }
+        });
+
+
+        app.get('/mytoys', async (req, res) => {
+            const result = await toyCollection.find({}).toArray()
+            res.send(result)
+        })
+
+        app.get('/mytoys/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await toyCollection.findOne(query)
+            res.send(result)
+        })
+
+        app.put('/mytoys/:id', async (req, res) => {
+            const id = req.params.id;
+            const toy = req.body;
+            const filter = { _id: new ObjectId(id) }
+            const options = { upsert: true }
+            const updatedToys = {
+                $set: {
+                    price: toy.price,
+                    avilableQuantity: toy.avilableQuantity,
+                    detail: toy.detail
+
+                }
+            }
+            const result = await toyCollection.updateOne(filter, updatedToys, options)
+            res.send(result)
+        })
+
+        app.delete('/mytoys/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await toyCollection.deleteOne(query)
+            res.send(result)
+        })
+
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+app.get('/', (req, res) => {
+    res.send('hello')
+})
 
 
 
