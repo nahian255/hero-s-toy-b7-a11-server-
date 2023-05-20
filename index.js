03b531155055f35cdac2681a1c59f5cb608404cc
nahian255 <nahimn253@gmail.com>
2023-05-20 10:03:04 +0600

    envdone

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(cors())
 app.use(express.json())
 
 
-const uri = "mongodb+srv://toydb:LH8ct1ZUDFjoCCMC@cluster0.lyu30gb.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.lyu30gb.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
